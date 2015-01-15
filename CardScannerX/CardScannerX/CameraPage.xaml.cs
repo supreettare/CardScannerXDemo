595996622c90fa48243df6ade4d5e7212041d13f
Supreet <tare.supreet@gmail.com>
2015-01-15 14:26:11 +0530

    updated the CamCard code

--- CardScannerX/CardScannerX/CameraPage.xaml.cs
+++ CardScannerX/CardScannerX/CameraPage.xaml.cs
@@ -1,8 +1,13 @@
 ﻿using Acr.XamForms.Mobile.Media;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
@@ -21,7 +26,7 @@ namespace CardScannerX
 
             if (DependencyService.Get<IMediaPicker>().IsCameraAvailable)
             {
-                var result = await DependencyService.Get<IMediaPicker>().TakePhoto();
+                var result = await DependencyService.Get<IMediaPicker>().PickPhoto();
                 this.OnPhotoReceived(result);
             }   
         }
@@ -37,18 +42,77 @@ namespace CardScannerX
             }
         }
 
+        int size = 0; 
 
-        private void OnPhotoReceived(IMediaFile file)
+
+        private async void OnPhotoReceived(IMediaFile file)
         {
-            if (file == null)
+            try
             {
-                //this.dialogs.Alert("Photo Cancelled");
+                if (file == null)
+                {
+                    //this.dialogs.Alert("Photo Cancelled");
+                }
+                else
+                {
+                    this.Photo = ImageSource.FromFile(file.Path);
+                    imgCaptured.Source = file.Path;
+                    byte[] fileBytes = ReadFully(file.GetStream());
+                    size = fileBytes.Length;
+
+                    var response = await Upload(fileBytes, size);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                this.Photo = ImageSource.FromFile(file.Path);
-                imgCaptured.Source = file.Path;
+
+                var m = ex.Message;
             }
+        }
+
+        public static byte[] ReadFully(Stream input)
+        {
+            byte[] buffer = new byte[16 * 1024];
+            using (MemoryStream ms = new MemoryStream())
+            {
+                int read;
+                while ((read = input.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    ms.Write(buffer, 0, read);
+                }
+                return ms.ToArray();
+            }
+        }
+
+        public async Task<HttpResponseMessage> Upload(byte[] image, int size)
+        {
+
+            try
+            {
+                var client = new HttpClient();
+                var requestContent = new MultipartFormDataContent();
+                //    here you can specify boundary if you need---^
+                
+                var imageContent = new ByteArrayContent(image);
+                imageContent.Headers.ContentType =
+                    MediaTypeHeaderValue.Parse("image/jpeg");
+                //client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("multipart/form-data,boundary=-------------------------acebdf13572468"));
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("text/x-vcard"));
+
+
+
+                requestContent.Add(imageContent, "image", "image.jpg");
+                string url = "http://bcr1.intsig.net/BCRService/BCR_VCF2?PIN=&user=supreet.tare@taritas.com&pass=6RPY9KPCCFGM54F9&lang=1&size=" + size;
+
+                return await client.PostAsync(url, requestContent);
+
+            }
+            catch (Exception ex)
+            {
+                var t = ex.Message;
+                return new HttpResponseMessage(System.Net.HttpStatusCode.BadRequest);
+            }
+            
         }
     }
 }
